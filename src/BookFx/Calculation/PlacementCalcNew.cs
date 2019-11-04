ef89aeb958cefff3f263c1e9d15acd34a868d3f5
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-04 19:50:33 +0300

    PlacementCalcNew

---
+++ src/BookFx/Calculation/PlacementCalcNew.cs
@@ -0,0 +1,31 @@
+﻿namespace BookFx.Calculation
+{
+    using BookFx.Cores;
+    using BookFx.Functional;
+    using Cache = System.Collections.Immutable.ImmutableDictionary<
+        (BookFx.Cores.BoxCore, BookFx.Calculation.Measure),
+        BookFx.Functional.Result<int>
+    >;
+
+    // todo rename to PlacementCalc
+    internal static class PlacementCalcNew
+    {
+        public static (Result<Placement> Result, Cache Cache) Perform(BoxCore box, Cache cache, Structure structure)
+        {
+            var firstRow = FirstRowCalc.Perform(box, cache, structure);
+            var firstCol = FirstColCalc.Perform(box, firstRow.Cache, structure);
+            var height = HeightCalc.Perform(box, firstCol.Cache, structure);
+            var width = WidthCalc.Perform(box, height.Cache, structure);
+
+            var result =
+                from r in firstRow.Result
+                from c in firstCol.Result
+                from h in height.Result
+                from w in width.Result
+                from placement in Placement.At(r, c, h, w)
+                select placement;
+
+            return (result, width.Cache);
+        }
+    }
+}
---
+++ src/BookFx/Calculation/WidthCalc.cs
@@ -0,0 +1,16 @@
+﻿namespace BookFx.Calculation
+{
+    using System;
+    using BookFx.Cores;
+    using BookFx.Functional;
+    using Cache = System.Collections.Immutable.ImmutableDictionary<
+        (BookFx.Cores.BoxCore, BookFx.Calculation.Measure),
+        BookFx.Functional.Result<int>
+    >;
+
+    internal static class WidthCalc
+    {
+        public static (Result<int> Result, Cache Cache) Perform(BoxCore box, Cache cache, Structure structure) =>
+            throw new NotImplementedException();
+    }
+}
