e1993566bdd07e587be50787ee21f2dd2c2f0023
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-04 19:51:17 +0300

    LayoutCalc.LayOutNew

--- src/BookFx/Calculation/LayoutCalc.cs
+++ src/BookFx/Calculation/LayoutCalc.cs
@@ -1,7 +1,12 @@
 ﻿namespace BookFx.Calculation
 {
+    using System;
     using BookFx.Cores;
     using BookFx.Functional;
+    using Cache = System.Collections.Immutable.ImmutableDictionary<
+        (BookFx.Cores.BoxCore, BookFx.Calculation.Measure),
+        BookFx.Functional.Result<int>
+    >;
 
     internal static class LayoutCalc
     {
@@ -17,5 +22,49 @@
             sheet.LayOut().ValueUnsafe();
 
         public static BoxCore LayOutUnsafe(this BoxCore box) => box.LayOut().ValueUnsafe();
+
+        // todo rename to LayOut
+        public static Result<BoxCore> LayOutNew(this BoxCore rootBox)
+        {
+            var structure = Structure.Create(rootBox);
+            var cache = Cache.Empty;
+            var (result, _) = LayOut(rootBox, cache, structure);
+            return result;
+        }
+
+        private static (Result<BoxCore>, Cache) LayOut(BoxCore box, Cache cache, Structure structure) =>
+            PlacementCalcNew
+                .Perform(box, cache, structure)
+                .Bind((placement, newCache) => box.Match(
+                    row: x => LayOutComposite(x, placement, newCache, structure),
+                    col: x => LayOutComposite(x, placement, newCache, structure),
+                    stack: x => LayOutComposite(x, placement, newCache, structure),
+                    value: x => (LayOutValue(x, placement), newCache),
+                    proto: x => (LayOutProto(x, placement), newCache)));
+
+        private static (Result<BoxCore>, Cache) LayOutComposite(
+            BoxCore box,
+            Placement placement,
+            Cache cache,
+            Structure structure)
+        {
+            var (childResults, newCache) = box
+                .Children
+                .AggMap(cache, (child, accCache) => LayOut(child, accCache, structure));
+
+            var result = childResults
+                .HarvestErrors()
+                .Map(children => box
+                    .With(children: children)
+                    .With(placement: placement));
+
+            return (result, newCache);
+        }
+
+        private static Result<BoxCore> LayOutValue(BoxCore box, Placement placement) =>
+            box.With(placement: placement);
+
+        private static Result<BoxCore> LayOutProto(BoxCore box, Placement placement) =>
+            throw new NotImplementedException(); // todo
     }
 }
