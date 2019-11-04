0d6d233e648e3bc33a3c79320af37d0ec46fc741
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-04 19:50:18 +0300

    ResultCacheExt

---
+++ src/BookFx/Calculation/ResultCacheExt.cs
@@ -0,0 +1,31 @@
+﻿namespace BookFx.Calculation
+{
+    using System;
+    using BookFx.Functional;
+    using static BookFx.Functional.F;
+    using Cache = System.Collections.Immutable.ImmutableDictionary<
+        (BookFx.Cores.BoxCore, BookFx.Calculation.Measure),
+        BookFx.Functional.Result<int>
+    >;
+
+    internal static class ResultCacheExt
+    {
+        public static (Result<TR>, Cache) Map<T, TR>(
+            this (Result<T> Result, Cache Cache) rc,
+            Func<T, Cache, (TR, Cache)> mapper) =>
+            rc.Result.Match(
+                invalid: errors => (Invalid(errors), rc.Cache),
+                valid: value =>
+                {
+                    var (newValue, newCache) = mapper(value, rc.Cache);
+                    return (Valid(newValue), newCache);
+                });
+
+        public static (Result<TR>, Cache) Bind<T, TR>(
+            this (Result<T> Result, Cache Cache) rc,
+            Func<T, Cache, (Result<TR>, Cache)> binder) =>
+            rc.Result.Match(
+                invalid: errors => (Invalid(errors), rc.Cache),
+                valid: value => binder(value, rc.Cache));
+    }
+}
