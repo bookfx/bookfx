e65a7dbd15412ba40942e609c32b9f6c7b23aba8
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-04 19:48:45 +0300

    AggMap

--- src/BookFx/Calculation/EnumerableResultCacheExt.cs
+++ src/BookFx/Calculation/EnumerableResultCacheExt.cs
@@ -11,6 +11,7 @@
 
     internal static class EnumerableResultCacheExt
     {
+        // todo via Map
         public static (Result<int>, Cache) Max<T>(
             this IEnumerable<T> list,
             (Result<int>, Cache) seed,
@@ -28,6 +29,7 @@
                     return (newResult, newCache);
                 });
 
+        // todo via Map
         public static (Result<int>, Cache) Sum<T>(
             this IEnumerable<T> list,
             (Result<int>, Cache) seed,
@@ -43,6 +45,20 @@
                         from currValue in currResult
                         select accValue + currValue;
                     return (newResult, newCache);
+                });
+
+        // todo AggMap?
+        public static (IEnumerable<Result<TR>>, Cache) AggMap<T, TR>(
+            this IEnumerable<T> list,
+            Cache cache,
+            Func<T, Cache, (Result<TR>, Cache)> selector) =>
+            list.Aggregate(
+                (Enumerable.Empty<Result<TR>>(), cache),
+                (acc, curr) =>
+                {
+                    var (accResult, accCache) = acc;
+                    var (currResult, newCache) = selector(curr, accCache);
+                    return (accResult.Append(currResult), newCache);
                 });
     }
 }
