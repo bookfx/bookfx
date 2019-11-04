f27f6ef417b59eeeb5c509ff403eeebc07d907b2
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-04 16:06:31 +0300

    EnumerableResultCacheExt

---
+++ src/BookFx/Calculation/EnumerableResultCacheExt.cs
@@ -0,0 +1,48 @@
+﻿namespace BookFx.Calculation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using BookFx.Functional;
+    using Cache = System.Collections.Immutable.ImmutableDictionary<
+        (BookFx.Cores.BoxCore, BookFx.Calculation.Measure),
+        BookFx.Functional.Result<int>
+    >;
+
+    internal static class EnumerableResultCacheExt
+    {
+        public static (Result<int>, Cache) Max<T>(
+            this IEnumerable<T> list,
+            (Result<int>, Cache) seed,
+            Func<T, Cache, (Result<int>, Cache)> selector) =>
+            list.Aggregate(
+                seed,
+                (acc, curr) =>
+                {
+                    var (accResult, accCache) = acc;
+                    var (currResult, newCache) = selector(curr, accCache);
+                    var newResult =
+                        from accValue in accResult
+                        from currValue in currResult
+                        select Math.Max(accValue, currValue);
+                    return (newResult, newCache);
+                });
+
+        public static (Result<int>, Cache) Sum<T>(
+            this IEnumerable<T> list,
+            (Result<int>, Cache) seed,
+            Func<T, Cache, (Result<int>, Cache)> selector) =>
+            list.Aggregate(
+                seed,
+                (acc, curr) =>
+                {
+                    var (accResult, accCache) = acc;
+                    var (currResult, newCache) = selector(curr, accCache);
+                    var newResult =
+                        from accValue in accResult
+                        from currValue in currResult
+                        select accValue + currValue;
+                    return (newResult, newCache);
+                });
+    }
+}
