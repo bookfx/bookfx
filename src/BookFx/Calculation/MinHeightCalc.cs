6922e2924efd2a2d48fc1c23f48f35bd74a6bc66
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-04 16:06:57 +0300

    Min*Calcs

---
+++ src/BookFx/Calculation/MinHeightCalc.cs
@@ -0,0 +1,45 @@
+﻿namespace BookFx.Calculation
+{
+    using BookFx.Cores;
+    using BookFx.Functional;
+    using static BookFx.Functional.F;
+    using Cache = System.Collections.Immutable.ImmutableDictionary<
+        (BookFx.Cores.BoxCore, BookFx.Calculation.Measure),
+        BookFx.Functional.Result<int>
+    >;
+
+    internal static class MinHeightCalc
+    {
+        public static (Result<int> Result, Cache Cache) MinHeight(BoxCore box, Cache cache) =>
+            cache.GetOrEval(
+                box,
+                Measure.MinHight,
+                () => box.Match(
+                    row: _ => RowMinHeight(box, cache),
+                    col: _ => ColMinHeight(box, cache),
+                    stack: _ => StackMinHeight(box, cache),
+                    value: _ => (ValueMinHeight(box), cache),
+                    proto: _ => (ProtoMinHeight(box), cache)));
+
+        private static (Result<int>, Cache) RowMinHeight(BoxCore box, Cache cache) =>
+            box.Children.Max(
+                seed: (Valid(0), cache),
+                selector: MinHeight);
+
+        private static (Result<int>, Cache) ColMinHeight(BoxCore box, Cache cache) =>
+            box.Children.Sum(
+                seed: (Valid(0), cache),
+                selector: MinHeight);
+
+        private static (Result<int>, Cache) StackMinHeight(BoxCore box, Cache cache) =>
+            box.Children.Max(
+                seed: (Valid(0), cache),
+                selector: MinHeight);
+
+        private static Result<int> ValueMinHeight(BoxCore box) =>
+            box.RowSpan.GetOrElse(1);
+
+        private static Result<int> ProtoMinHeight(BoxCore box) =>
+            box.Proto.Bind(x => x.Range).ValueUnsafe().Rows;
+    }
+}
---
+++ src/BookFx/Calculation/MinWidthCalc.cs
@@ -0,0 +1,45 @@
+﻿namespace BookFx.Calculation
+{
+    using BookFx.Cores;
+    using BookFx.Functional;
+    using static BookFx.Functional.F;
+    using Cache = System.Collections.Immutable.ImmutableDictionary<
+        (BookFx.Cores.BoxCore, BookFx.Calculation.Measure),
+        BookFx.Functional.Result<int>
+    >;
+
+    internal static class MinWidthCalc
+    {
+        public static (Result<int> Result, Cache Cache) MinWidth(BoxCore box, Cache cache) =>
+            cache.GetOrEval(
+                box,
+                Measure.MinWidth,
+                () => box.Match(
+                    row: _ => RowMinWidth(box, cache),
+                    col: _ => ColMinWidth(box, cache),
+                    stack: _ => StackMinWidth(box, cache),
+                    value: _ => (ValueMinWidth(box), cache),
+                    proto: _ => (ProtoMinWidth(box), cache)));
+
+        private static (Result<int>, Cache) RowMinWidth(BoxCore box, Cache cache) =>
+            box.Children.Sum(
+                seed: (Valid(0), cache),
+                selector: MinWidth);
+
+        private static (Result<int>, Cache) ColMinWidth(BoxCore box, Cache cache) =>
+            box.Children.Max(
+                seed: (Valid(0), cache),
+                selector: MinWidth);
+
+        private static (Result<int>, Cache) StackMinWidth(BoxCore box, Cache cache) =>
+            box.Children.Max(
+                seed: (Valid(0), cache),
+                selector: MinWidth);
+
+        private static Result<int> ValueMinWidth(BoxCore box) =>
+            box.ColSpan.GetOrElse(1);
+
+        private static Result<int> ProtoMinWidth(BoxCore box) =>
+            box.Proto.Bind(x => x.Range).ValueUnsafe().Columns;
+    }
+}
