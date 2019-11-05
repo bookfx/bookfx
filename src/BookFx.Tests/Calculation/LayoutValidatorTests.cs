eb7f63147cd25d50066ffb5a009b8e7c0e07f7aa
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-05 08:02:16 +0300

    LayoutValidator

---
+++ src/BookFx.Tests/Calculation/LayoutValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿namespace BookFx.Tests.Calculation
+{
+    using BookFx.Calculation;
+    using BookFx.Cores;
+    using BookFx.Epplus;
+    using FluentAssertions;
+    using Xunit;
+
+    public class LayoutValidatorTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(Constraint.MaxRow + 1)]
+        public void Position_InvalidRow_Invalid(int value) =>
+            LayoutValidator.Position(GetBoxAt(value, 1)).IsValid.Should().BeFalse();
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(Constraint.MaxRow + 1)]
+        public void Position_InvalidCol_Invalid(int value) =>
+            LayoutValidator.Position(GetBoxAt(1, value)).IsValid.Should().BeFalse();
+
+        private static BoxCore GetBoxAt(int row, int col)
+        {
+            var position = Position.AtUnsafe(row, col);
+            var dimension = Dimension.Of(1, 1);
+            return BoxCore.Create(BoxType.Value).With(placement: Placement.At(position, dimension));
+        }
+    }
+}
---
+++ src/BookFx/Calculation/LayoutValidator.cs
@@ -0,0 +1,26 @@
+﻿namespace BookFx.Calculation
+{
+    using BookFx.Cores;
+    using BookFx.Epplus;
+    using BookFx.Functional;
+    using static BookFx.Functional.F;
+
+    internal static class LayoutValidator
+    {
+        public static Tee<BoxCore> Validate =>
+            box => box
+                .SelfAndDescendants()
+                .Map(Position.Invoke)
+                .HarvestErrors()
+                .Map(_ => box);
+
+        public static Tee<BoxCore> Position =>
+            box =>
+                box.Placement.Position.Row < 1 ||
+                box.Placement.Position.Row > Constraint.MaxRow ||
+                box.Placement.Position.Col < 1 ||
+                box.Placement.Position.Col > Constraint.MaxColumn
+                    ? Errors.Position.IsInvalid(box.Placement.Position.Row, box.Placement.Position.Col)
+                    : Valid(box);
+    }
+}
