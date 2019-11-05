d8467e82392b482460ca77df01bb8747e2bd5972
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-05 12:06:57 +0300

    EnumerableScExt.Traverse

---
+++ src/BookFx/Functional/EnumerableScExt.cs
@@ -0,0 +1,18 @@
+﻿namespace BookFx.Functional
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal static class EnumerableScExt
+    {
+        // todo test
+        public static Sc<TS, IEnumerable<TR>> Traverse<TS, TV, TR>(this IEnumerable<TV> xs, Func<TV, Sc<TS, TR>> f) =>
+            xs.Aggregate(
+                seed: Sc<TS>.Return(Enumerable.Empty<TR>()),
+                func: (sc, x) =>
+                    from rs in sc
+                    from r in f(x)
+                    select rs.Append(r));
+    }
+}
