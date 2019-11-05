321ef5ae58049344b5d6ff75067038ffcb23979a
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-05 10:55:14 +0300

    Sc param docs

--- src/BookFx/Functional/Sc{TS,TV}.cs
+++ src/BookFx/Functional/Sc{TS,TV}.cs
@@ -3,5 +3,9 @@
     /// <summary>
     /// Stateful computation.
     /// </summary>
+    /// <typeparam name="TS">State type.</typeparam>
+    /// <typeparam name="TV">Value type.</typeparam>
+    /// <param name="state">State.</param>
+    /// <returns>(Value, State) tuple.</returns>
     public delegate (TV Value, TS State) Sc<TS, TV>(TS state);
 }
