d78cb6eab1fdfe673b3a4e4cc28402c8eb604b2c
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-05 11:38:03 +0300

    ScComposition

---
+++ src/BookFx/Functional/ScComposition.cs
@@ -0,0 +1,23 @@
+﻿namespace BookFx.Functional
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using JetBrains.Annotations;
+
+    internal static class ScComposition
+    {
+        // todo test
+        [Pure]
+        public static Sc<TS, TV> Compose<TS, TV>(
+            this IEnumerable<Sc<TS, TV>> computations,
+            TV seed,
+            Func<TV, TV, TV> f) =>
+            computations.Aggregate(
+                seed: Sc<TS>.Return(seed),
+                func: (accSc, currSc) =>
+                    from acc in accSc
+                    from curr in currSc
+                    select f(acc, curr));
+    }
+}
