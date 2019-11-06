ce29266ff96887d7bc8ed605c5bc3f75ebd90d70
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-06 09:31:18 +0300

    MinHeightCalcTests

---
+++ src/BookFx.Tests/Calculation/MinHeightCalcTests.cs
@@ -0,0 +1,27 @@
+﻿namespace BookFx.Tests.Calculation
+{
+    using BookFx.Calculation;
+    using BookFx.Functional;
+    using FluentAssertions;
+    using Xunit;
+
+    public class MinHeightCalcTests
+    {
+        [Fact]
+        public void MinHeight_ColWith2Children_Expected()
+        {
+            // A
+            // B
+            var box = Make
+                .Col(
+                    Make.Value("A").SpanRows(3),
+                    Make.Value("B").SpanRows(5)
+                )
+                .Get;
+
+            var result = MinHeightCalc.MinHeight(box).Run(Cache.Empty);
+
+            result.Should().Be(8);
+        }
+    }
+}
