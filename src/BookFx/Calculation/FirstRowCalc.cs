329c3e78ce4dc21331c2b5f473a3592ea57bba41
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-06 15:20:03 +0300

    FirstRow

--- src/BookFx/Calculation/FirstRowCalc.cs
+++ src/BookFx/Calculation/FirstRowCalc.cs
@@ -3,10 +3,33 @@
     using System;
     using BookFx.Cores;
     using BookFx.Functional;
+    using static BookFx.Functional.Sc<Cache>;
+    using static HeightCalc;
 
     internal static class FirstRowCalc
     {
         public static Sc<Cache, int> FirstRow(BoxCore box, Structure structure) =>
-            throw new NotImplementedException();
+            structure
+                .Parent(box)
+                .Map(parent => parent
+                    .Match(
+                        row: _ => FirstRow(parent, structure),
+                        col: _ => structure
+                            .Prev(box)
+                            .Map(prev =>
+                                from prevFirstRow in FirstRow(prev, structure)
+                                from prevHeight in Height(prev, structure)
+                                select prevFirstRow + prevHeight + 1)
+                            .OrElse(FirstRow(parent, structure))
+                            .GetOrElse(ScOf(1)),
+                        stack: _ => FirstRow(parent, structure),
+                        value: _ => throw new InvalidOperationException(),
+                        proto: _ =>
+                            from parentFirstRow in FirstRow(parent, structure)
+                            let slotRelativeRow = structure.Slot(box).Position.ValueUnsafe().Row
+                            select parentFirstRow + slotRelativeRow - 1
+                    )
+                )
+                .GetOrElse(ScOf(1));
     }
 }
