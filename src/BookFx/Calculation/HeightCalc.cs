32be6ce99330ca46ea56d7be4024d872d4f9eb92
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-06 11:25:33 +0300

    HeightCalc.OfValue

--- src/BookFx/Calculation/HeightCalc.cs
+++ src/BookFx/Calculation/HeightCalc.cs
@@ -6,7 +6,20 @@
 
     internal static class HeightCalc
     {
-        public static Sc<Cache, int> Height(BoxCore box, Structure structure) =>
-            throw new NotImplementedException();
+        public static Sc<Cache, int> Height(BoxCore box, Structure structure) => throw new NotImplementedException();
+
+        private static Sc<Cache, int> OfValue(BoxCore box, Structure structure) =>
+            box
+                .RowSpan
+                .Map(Sc<Cache>.Return)
+                .OrElse(() => structure
+                    .Parent(box)
+                    .Map(
+                        row: MinHeightCalc.MinHeight,
+                        col: _ => Sc<Cache>.Return(1),
+                        stack: MinHeightCalc.MinHeight,
+                        value: _ => throw new InvalidOperationException(),
+                        proto: _ => Sc<Cache>.Return(1)))
+                .GetOrElse(Sc<Cache>.Return(1));
     }
 }
