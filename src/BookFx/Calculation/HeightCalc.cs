254d74691d7ddc265a8a731fb58dbb3939adbfb8
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-06 11:45:57 +0300

    WidthCalc, HeightCalc

--- src/BookFx/Calculation/HeightCalc.cs
+++ src/BookFx/Calculation/HeightCalc.cs
@@ -8,7 +8,23 @@
 
     internal static class HeightCalc
     {
-        public static Sc<Cache, int> Height(BoxCore box, Structure structure) => throw new NotImplementedException();
+        public static Sc<Cache, int> Height(BoxCore box, Structure structure) =>
+            from cache in GetState
+            from result in cache.GetOrCompute(
+                key: (box, Measure.Hight),
+                sc: () => box.Match(
+                    row: _ => OfRow(box),
+                    col: _ => OfCol(box),
+                    stack: _ => OfStack(box),
+                    value: _ => OfValue(box, structure),
+                    proto: _ => OfProto(box)))
+            select result;
+
+        private static Sc<Cache, int> OfRow(BoxCore box) => MinHeight(box);
+
+        private static Sc<Cache, int> OfCol(BoxCore box) => MinHeight(box);
+
+        private static Sc<Cache, int> OfStack(BoxCore box) => MinHeight(box);
 
         private static Sc<Cache, int> OfValue(BoxCore box, Structure structure) =>
             box
@@ -23,5 +39,7 @@
                         value: _ => throw new InvalidOperationException(),
                         proto: _ => ScOf(1)))
                 .GetOrElse(ScOf(1));
+
+        private static Sc<Cache, int> OfProto(BoxCore box) => MinHeight(box);
     }
 }
--- src/BookFx/Calculation/WidthCalc.cs
+++ src/BookFx/Calculation/WidthCalc.cs
@@ -9,7 +9,22 @@
     internal static class WidthCalc
     {
         public static Sc<Cache, int> Width(BoxCore box, Structure structure) =>
-            throw new NotImplementedException();
+            from cache in GetState
+            from result in cache.GetOrCompute(
+                key: (box, Measure.Width),
+                sc: () => box.Match(
+                    row: _ => OfRow(box),
+                    col: _ => OfCol(box),
+                    stack: _ => OfStack(box),
+                    value: _ => OfValue(box, structure),
+                    proto: _ => OfProto(box)))
+            select result;
+
+        private static Sc<Cache, int> OfRow(BoxCore box) => MinWidth(box);
+
+        private static Sc<Cache, int> OfCol(BoxCore box) => MinWidth(box);
+
+        private static Sc<Cache, int> OfStack(BoxCore box) => MinWidth(box);
 
         private static Sc<Cache, int> OfValue(BoxCore box, Structure structure) =>
             box
@@ -24,5 +39,7 @@
                         value: _ => throw new InvalidOperationException(),
                         proto: _ => ScOf(1)))
                 .GetOrElse(ScOf(1));
+
+        private static Sc<Cache, int> OfProto(BoxCore box) => MinWidth(box);
     }
 }
