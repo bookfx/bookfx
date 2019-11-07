e89bbe1d72a0c7072151c563796fd129d2212cdb
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-07 12:32:30 +0300

    S4BigTable

--- src/BookFx.Usage/Program.cs
+++ src/BookFx.Usage/Program.cs
@@ -9,6 +9,7 @@
             ResultStore.Save(S1Table.Create(), $"{nameof(S1Table)}.xlsx");
             ResultStore.Save(S2Style.Create(), $"{nameof(S2Style)}.xlsx");
             ResultStore.Save(S3Calendar.Create(DateTime.Now.Year), $"{nameof(S3Calendar)}.xlsx");
+            ResultStore.Save(S4BigTable.Create(), $"{nameof(S4BigTable)}.xlsx");
         }
     }
 }
---
+++ src/BookFx.Usage/S4BigTable.cs
@@ -0,0 +1,26 @@
+﻿namespace BookFx.Usage
+{
+    using System.Linq;
+
+    public static class S4BigTable
+    {
+        private const int RowCount = 1000; // About 1.5 seconds per 1K rows.
+        private const int ColCount = 10;
+
+        public static byte[] Create() =>
+            Enumerable
+                .Range(1, RowCount)
+                .Select(GetRow)
+                .ToCol()
+                .ToSheet()
+                .ToBook()
+                .ToBytes();
+
+        private static Box GetRow(int rowNumber) =>
+            Enumerable
+                .Range(1, ColCount)
+                .Select(col => (rowNumber - 1) * ColCount + col)
+                .Select(Make.Value)
+                .ToRow();
+    }
+}
