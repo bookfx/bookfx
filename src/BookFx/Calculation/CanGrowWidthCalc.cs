d0834f201f5e241d4e7a626d7d234df42e5150b6
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-08 20:20:25 +0300

    use *AutoSpan in calcs

--- src/BookFx/Calculation/CanGrowWidthCalc.cs
+++ src/BookFx/Calculation/CanGrowWidthCalc.cs
@@ -2,13 +2,14 @@
 {
     using System.Linq;
     using BookFx.Cores;
+    using BookFx.Functional;
 
     internal static class CanGrowWidthCalc
     {
         public static bool CanGrowWidth(this BoxCore box, Layout layout) =>
             layout.Cache.CanGrowWidth(
                 box,
-                () => box.Match(
+                () => AutoSpan(box, layout) && box.Match(
                     row: _ => DependingOnChildren(box, layout),
                     col: _ => DependingOnChildren(box, layout),
                     stack: _ => DependingOnChildren(box, layout),
@@ -17,5 +18,14 @@
 
         private static bool DependingOnChildren(BoxCore box, Layout layout) =>
             box.Children.Any(child => child.CanGrowWidth(layout));
+
+        private static bool AutoSpan(BoxCore box, Layout layout) =>
+            box
+                .ColAutoSpan
+                .OrElse(layout
+                    .Relations
+                    .Parent(box)
+                    .Map(parent => AutoSpan(parent, layout)))
+                .GetOrElse(true);
     }
 }
