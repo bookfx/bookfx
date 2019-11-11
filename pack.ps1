7ba5a12138a97c54aff610611de3ee62b81cbda4
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-11 16:30:04 +0300

    detect version with git describe

--- pack.ps1
+++ pack.ps1
@@ -1,4 +1,23 @@
 $ErrorActionPreference = "Stop"
 
-& dotnet pack .\src\BookFx\BookFx.csproj --configuration Release --output $pwd\.packs
-& dotnet pack .\src\BookFx\BookFx.csproj --configuration Release --include-symbols -p:SymbolPackageFormat=snupkg --output $pwd\.packs
+$version = git describe --tags
+
+if ($version.StartsWith("v")) {
+    $version = $version.Substring(1)
+}
+
+Write-Host "Version: $version"
+
+if ($version.Contains("-g"))
+{
+    [string]$confirmation = Read-Host "Continue? [y/N]"
+
+    if (-Not ($confirmation -match "^[yY]$"))
+    {
+        write-host "Cancelled."
+        Exit
+    }
+}
+
+& dotnet pack .\src\BookFx\BookFx.csproj --configuration Release -p:Version=$version --output $pwd\.packs
+& dotnet pack .\src\BookFx\BookFx.csproj --configuration Release -p:Version=$version --include-symbols -p:SymbolPackageFormat=snupkg --output $pwd\.packs
