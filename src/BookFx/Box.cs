b3fbb4f260757cc8e84801eb2484a28223eed4b0
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-15 17:14:59 +0300

    xml docs. Box

--- src/BookFx/Box.cs
+++ src/BookFx/Box.cs
@@ -8,11 +8,17 @@
     using static BookFx.Functional.F;
     using static BookFx.Make;
 
+    /// <summary>
+    /// Box is the building block of a sheet.
+    /// </summary>
     [PublicAPI]
     public abstract class Box
     {
         private protected Box(BoxCore core) => Get = core;
 
+        /// <summary>
+        /// Gets properties of the box.
+        /// </summary>
         public BoxCore Get { get; }
 
         [Pure]
@@ -45,6 +51,10 @@
         [Pure]
         public static implicit operator Box(DateTime value) => Value(value);
 
+        /// <summary>
+        /// Define a name of the range.
+        /// </summary>
+        /// <param name="name">A range name.</param>
         [Pure]
         public Box Name(string name) => Get.With(name: Some(name));
 
@@ -68,47 +78,89 @@
         [Pure]
         public Box AutoSpanCols(bool autoSpanCols) => Get.With(colAutoSpan: autoSpanCols);
 
+        /// <summary>
+        /// Define a style.
+        /// </summary>
         [Pure]
         public Box Style(BoxStyle style) => Get.With(style: style.Get);
 
+        /// <summary>
+        /// Define heights of rows.
+        /// </summary>
+        /// <param name="sizes">Sizes of rows.</param>
         [Pure]
         public Box SizeRows(IEnumerable<TrackSize> sizes) => Get.With(rowSizes: sizes);
 
+        /// <summary>
+        /// Define heights of rows.
+        /// </summary>
+        /// <param name="size">A size of the first row.</param>
+        /// <param name="others">Sizes of other rows.</param>
         [Pure]
         public Box SizeRows(TrackSize size, params TrackSize[] others) =>
             Get.With(rowSizes: others.Prepend(size));
 
+        /// <summary>
+        /// Define widths of columns.
+        /// </summary>
+        /// <param name="sizes">Sizes of columns.</param>
         [Pure]
         public Box SizeCols(IEnumerable<TrackSize> sizes) => Get.With(colSizes: sizes);
 
+        /// <summary>
+        /// Define widths of columns.
+        /// </summary>
+        /// <param name="size">A size of the first column.</param>
+        /// <param name="others">Sizes of other columns.</param>
         [Pure]
         public Box SizeCols(TrackSize size, params TrackSize[] others) =>
             Get.With(colSizes: others.Prepend(size));
 
+        /// <summary>
+        /// Define print area by the box.
+        /// </summary>
         [Pure]
         public Box SetPrintArea() => Get.With(isPrintArea: true);
 
+        /// <summary>
+        /// Hide rows.
+        /// </summary>
         [Pure]
         public Box HideRows() => Get.With(areRowsHidden: true);
 
+        /// <summary>
+        /// Hide columns.
+        /// </summary>
         [Pure]
         public Box HideCols() => Get.With(areColsHidden: true);
 
+        /// <summary>
+        /// Freeze the box range.
+        /// </summary>
         [Pure]
         public Box Freeze() => Get.With(areRowsFrozen: true, areColsFrozen: true);
 
+        /// <summary>
+        /// Freeze rows of the box.
+        /// </summary>
         [Pure]
         public Box FreezeRows() => Get.With(areRowsFrozen: true);
 
+        /// <summary>
+        /// Freeze columns of the box.
+        /// </summary>
         [Pure]
         public Box FreezeCols() => Get.With(areColsFrozen: true);
 
         /// <summary>
-        /// Adds auto filter to the box bottom.
+        /// Add auto filter to the lower row of the box.
         /// </summary>
         [Pure]
         public Box AutoFilter() => Get.With(isAutoFilter: true);
 
+        /// <summary>
+        /// Make a <see cref="Sheet"/> with the root box.
+        /// </summary>
         [Pure]
         public Sheet ToSheet() => Sheet(this);
     }
