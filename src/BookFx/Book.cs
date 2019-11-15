25698d3fbe564d95b609ca91d0cc913f3117bd6c
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-15 17:14:43 +0300

    xml docs. Book

--- src/BookFx/Book.cs
+++ src/BookFx/Book.cs
@@ -12,19 +12,33 @@
     [PublicAPI]
     public sealed class Book
     {
+        /// <summary>
+        /// The empty book.
+        /// </summary>
         public static readonly Book Empty = BookCore.Empty;
 
         private Book(BookCore core) => Get = core;
 
+        /// <summary>
+        /// Gets properties of the book.
+        /// </summary>
         public BookCore Get { get; }
 
         [Pure]
         public static implicit operator Book(BookCore core) => new Book(core);
 
+        /// <summary>
+        /// Add sheet(s) to the book.
+        /// </summary>
+        /// <param name="sheet">The first sheet.</param>
+        /// <param name="sheets">Other sheets.</param>
         [Pure]
         public Book Add(Sheet sheet, params Sheet[] sheets) =>
             Add(sheets.Prepend(sheet));
 
+        /// <summary>
+        /// Add sheet(s) to the book.
+        /// </summary>
         [Pure]
         public Book Add(IEnumerable<Sheet> sheets) => Get.Add(sheets.Map(x => x.Get));
     }
