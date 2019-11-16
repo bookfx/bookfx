00736ff905295326f2ed1fba09797a320095c9bf
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-16 09:42:47 +0300

    xml docs. BoxType

--- src/BookFx/BoxType.cs
+++ src/BookFx/BoxType.cs
@@ -1,7 +1,12 @@
-﻿namespace BookFx
+﻿#pragma warning disable 1591 // CS1591: Missing XML comment for publicly visible type or member
+
+namespace BookFx
 {
     using JetBrains.Annotations;
 
+    /// <summary>
+    /// Box type.
+    /// </summary>
     [PublicAPI]
     public enum BoxType : byte
     {
