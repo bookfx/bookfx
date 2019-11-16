ab1287b36fed9f70436d76feaf3f1a4e488bb694
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-16 10:04:03 +0300

    xml docs. InvalidBookException

--- src/BookFx/InvalidBookException.cs
+++ src/BookFx/InvalidBookException.cs
@@ -3,6 +3,9 @@
     using System;
     using JetBrains.Annotations;
 
+    /// <summary>
+    /// The only exception of BookFx, which is throws then the model has validation errors.
+    /// </summary>
     [PublicAPI]
     public sealed class InvalidBookException : Exception
     {
