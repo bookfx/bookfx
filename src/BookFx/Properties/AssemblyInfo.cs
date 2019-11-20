a77c31a8599e03c6fd2f50f3e549d9c9e713cb68
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-20 12:39:33 +0300

    mark assemblies with ComVisible

--- src/BookFx/Properties/AssemblyInfo.cs
+++ src/BookFx/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+[assembly: ComVisible(true)]
 [assembly: InternalsVisibleTo("BookFx.Benchmark")]
 [assembly: InternalsVisibleTo("BookFx.Tests")]
