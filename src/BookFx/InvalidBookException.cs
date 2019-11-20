66e449b1c4cadab6eb39cae98b8f0b17f0c2a2f6
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-20 11:17:37 +0300

    serialization patter for exception

--- src/BookFx/InvalidBookException.cs
+++ src/BookFx/InvalidBookException.cs
@@ -1,16 +1,23 @@
 ﻿namespace BookFx
 {
     using System;
+    using System.Runtime.Serialization;
     using JetBrains.Annotations;
 
     /// <summary>
     /// The only exception of BookFx, which is throws then the model has validation errors.
     /// </summary>
     [PublicAPI]
+    [Serializable]
     public sealed class InvalidBookException : Exception
     {
         internal InvalidBookException(Errors.Book.AggregateError error)
             : base(error.ToString())
+        {
+        }
+
+        private InvalidBookException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
