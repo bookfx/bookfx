86101a739a4d1c88fcbe18a6916cbc38221f2d58
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-20 13:22:23 +0300

    assemblies are not CLS compliant

--- src/BookFx/Properties/AssemblyInfo.cs
+++ src/BookFx/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: CLSCompliant(true)]
+[assembly: CLSCompliant(false)]
 [assembly: ComVisible(true)]
 [assembly: InternalsVisibleTo("BookFx.Benchmark")]
 [assembly: InternalsVisibleTo("BookFx.Tests")]
