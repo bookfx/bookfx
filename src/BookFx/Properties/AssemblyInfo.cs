44c63ec8531605eb9a987b9f5ebdfdee72924774
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-20 12:49:33 +0300

    mark assemblies with CLSCompliant(true)

--- src/BookFx/Properties/AssemblyInfo.cs
+++ src/BookFx/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: CLSCompliant(true)]
 [assembly: ComVisible(true)]
 [assembly: InternalsVisibleTo("BookFx.Benchmark")]
 [assembly: InternalsVisibleTo("BookFx.Tests")]
