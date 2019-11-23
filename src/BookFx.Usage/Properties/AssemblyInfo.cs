4a626d06e8fd10707b68c3412b6290d7011be2ad
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-23 11:02:34 +0300

    CLSCompliant(true)

--- src/BookFx.Usage/Properties/AssemblyInfo.cs
+++ src/BookFx.Usage/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-[assembly: CLSCompliant(false)]
+[assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
--- src/BookFx/Properties/AssemblyInfo.cs
+++ src/BookFx/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: CLSCompliant(false)]
+[assembly: CLSCompliant(true)]
 [assembly: ComVisible(true)]
 [assembly: InternalsVisibleTo("BookFx.Benchmark")]
 [assembly: InternalsVisibleTo("BookFx.Tests")]
