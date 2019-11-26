b62ae60ce63628f87b49263261918aec19265a75
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-11-26 20:21:55 +0300

    PageOrientation

---
+++ src/BookFx/PageOrientation.cs
@@ -0,0 +1,21 @@
+﻿namespace BookFx
+{
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Page orientation.
+    /// </summary>
+    [PublicAPI]
+    public enum PageOrientation
+    {
+        /// <summary>
+        /// Portrait orientation.
+        /// </summary>
+        Portrait,
+
+        /// <summary>
+        /// Landscape orientation.
+        /// </summary>
+        Landscape,
+    }
+}
