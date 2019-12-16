2f5aa7b1cfe133f9a211e81ca593ee73c779be50
Zhenya Gusev <45536778+zhenyagusev@users.noreply.github.com>
2019-12-16 11:31:13 +0300

    S5ProtoSheet example

--- src/BookFx.Usage/Program.cs
+++ src/BookFx.Usage/Program.cs
@@ -10,6 +10,7 @@
             ResultStore.Save(S2Style.Create(), $"{nameof(S2Style)}.xlsx");
             ResultStore.Save(S3Calendar.Create(DateTime.Now.AddMonths(2).Year), $"{nameof(S3Calendar)}.xlsx");
             ResultStore.Save(S4BigTable.Create(), $"{nameof(S4BigTable)}.xlsx");
+            ResultStore.Save(S5ProtoSheet.Create(), $"{nameof(S5ProtoSheet)}.xlsx");
         }
     }
 }
